9057b775ef3d3a8435509e4cd0d0144510c33a36
Alex Iverson <alexjiverson@gmail.com>
2025-05-21 01:17:56 -0700

    Update flake to more recent versions

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     luvitPkgs = {
       url = "github:aiverson/luvit-nix";
-      flake = false;
+      #flake = false;
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, luvitPkgs }: {
+  outputs = inputs@{ self, nixpkgs, luvitPkgs, flake-utils }: {
     packages.x86_64-linux.aiverson-me =
       let pkgs = import nixpkgs {
             system = "x86_64-linux";
           };
-          luvit = pkgs.callPackage(inputs.luvitPkgs) {};
+          #luvit = pkgs.callPackage(inputs.luvitPkgs) {};
           lib = pkgs.lib;
-      in luvit.makeLitPackage {
+      in luvitPkgs.lib.x86_64-linux.makeLitPackage {
         pname = "aiverson.me";
         version = "0.0.2";
+        litSha256 = "sha256-ku6Y95Uw3q6eTXQw4XlZq8TBAG1Vi36YuNVZRwDyOM8=";
 
         src = lib.sourceFilesBySuffices ./. [
           ".lua"
